310c8c78557c8993f6099b4d7fb34cf90e873376
carlyrobison <carly.ellen.robison@gmail.com>
2017-03-10 17:59:23 -0800

    did genre histograms and prepared submission

---
+++ sub/data_io.py
@@ -0,0 +1,84 @@
+# importing data from the input files
+import numpy as np
+
+genre_labels = ['Unknown', 'Action', 'Adventure', 'Animation', 'Childrens', 'Comedy', 'Crime', 'Documentary', 'Drama', 'Fantasy', 'FilmNoir', 'Horror', 'Musical', 'Mystery', 'Romance', 'SciFi', 'Thriller', 'War', 'Western']
+
+# the graduate, the right stuff, patton, et, die hard, psycho,
+# star trek the motion picture, jaws, aliens, terminator
+selected_movies = ['197', '193', '205', '423', '144', '185', '449', '234', '176', '195']
+
+star_movies = ['50', '62', '124', '146', '222', '227', '228', '229', '230', '271', '380', '449', '1061', '1068', '1265', '1293', '1464', '172', '181']
+
+def read_movies(filename):
+    '''
+    Gets movies and their genres from the given file.
+    '''
+    movie_names = {} # movie ID to name
+    movie_genres = {} # indexed by movie ID
+    genres = {} # genre name to set of IDs
+
+    # create the genre fields
+    for g in genre_labels:
+        genres[g] = set([])
+
+    with open(filename, 'rU') as f:
+        for line in f:
+            d = line.strip('\n').split('\t')
+
+            # extract stuff
+            movie_id = d[0]
+            title = d[1]
+            genre = [genre_labels[i] for i in range(len(genre_labels)) if d[i + 2] == '1']
+
+            # add the info to the relevant db
+            movie_names[movie_id] = title
+            movie_genres[movie_id] = genre
+            for g in genre:
+                genres[g].add(movie_id)
+
+    # return it
+    return movie_names, movie_genres, genres
+
+def read_ratings(filename, movie_names):
+    '''
+    Gets users and their ratings for movies from the given file
+    '''
+    Y = [] # the input data
+
+    movie_ratings = {}
+
+    # aggregate ratings by movie
+    for m in movie_names.keys():
+        movie_ratings[m] = []
+
+    with open(filename, 'rU') as f:
+        for line in f:
+            d = line.strip('\n').split('\t')
+
+            # extract stuff
+            user_id = int(d[0])
+            movie_id = d[1]
+            rating = int(d[2])
+
+            # add the info to the relevant db
+            Y.append([user_id, int(movie_id), rating])
+            movie_ratings[movie_id].append(rating)
+
+    # return it
+    return np.asarray(Y), movie_ratings
+
+def save_matrix(V, filename):
+    '''Dump trained matrix into a file'''
+    np.savetxt(filename, V, fmt='%f')
+
+def load_matrix(filename):
+    '''Load dumped matrix from file'''
+    return np.loadtxt(filename, dtype=float)
+
+
+
+if __name__ == '__main__':
+    movie_names, movie_genres, genres  = read_movies('movies.txt')
+    Y, movie_ratings = read_ratings('data.txt', movie_names)
+    #print "movies:", [movies[d] for d in movies.keys()[:10]]
+    # print "genres:", genres[:10]
---
+++ sub/matrixviz.py
@@ -0,0 +1,100 @@
+# matrix factorization!
+
+import data_io
+import prob2utils as svd
+import numpy as np
+import basic_histograms as stats
+import diagrams as diag
+import random
+
+k = 20 # size of matrix
+
+
+def get_internal_matrices(Y, K):
+    '''Returns U and V from factorizing Y.'''
+    M = max([d[0] for d in Y]) + 1 # number of users
+    N = max([d[1] for d in Y]) + 1 # number of movies
+
+    reg = .1 # used data set from last week
+    eta = 0.01 # initially .03
+    U, V, err = svd.train_model(M, N, K, eta, reg, Y)
+    print err
+    print V
+    return U, V
+
+def get_projection(V):
+    '''Gets the projection of V into the two dimensions with the most variation in the data.'''
+    A, E, B = np.linalg.svd(V)
+    Vproj = np.dot(A.transpose()[:2], V)
+    return Vproj.transpose()
+
+def get_movie_point(proj, movie_id):
+    # print proj[movie_id]
+    pt = proj[movie_id]
+    return pt[0], pt[1]
+
+def plot_movie_ids(title, ids, X):
+    movie_data = []
+    for m in ids:
+        x, y = get_movie_point(X, int(m))
+        movie_data.append((ensure_unicode(movie_names[m]), x, y))
+
+    diag.plot_movies(movie_data, title)
+
+def plot_popular(X, movie_names, movie_ratings):
+    mvs = stats.top_ten_popular(movie_ratings)
+    print "most popular movies:", [movie_names[m] for m in mvs]
+
+    plot_movie_ids("Most Popular Movies", mvs, X)
+
+def plot_highest_rated(X, movie_names, movie_ratings):
+    mvs = stats.top_ten_rated(movie_ratings)
+    print "highest rated movies:", [movie_names[m] for m in mvs]
+
+    plot_movie_ids("Highest Rated Movies", mvs, X)
+
+def plot_genres(X, movie_names, movie_ratings, genres):
+    for k in genres.keys():
+        v = list(genres[k])
+        num_samples = min(20, len(v))
+        plot_movie_ids("Genre: " + str(k), random.sample(list(v), num_samples), X)
+
+def ensure_unicode(v): # needed bc weird errors
+    if isinstance(v, str):
+        v = v.decode('latin-1')
+    return unicode(v)  # convert anything not a string to unicode too
+
+def plot_handpicked(X, movie_names, movie_ratings):
+    plot_movie_ids("Handpicked", data_io.selected_movies, X)
+
+def plot_star(X, movie_names, movie_ratings):
+    plot_movie_ids("Movies with Star in Title", data_io.star_movies, X)
+
+
+if __name__ == '__main__':
+    movie_names, movie_genres, genres  = data_io.read_movies('movies.txt')
+    Y, movie_ratings = data_io.read_ratings('data.txt', movie_names)
+    
+    # #if we're calculating U and V again. Otherwise just read from the file.
+    # U, V = get_internal_matrices(Y, k)
+    # data_io.save_matrix(U, 'savedU.txt')
+    # data_io.save_matrix(V, 'savedV.txt')
+
+    # #if we're calculating the projection again.
+    # Vnew = data_io.load_matrix('savedV.txt')
+    # Vproj = get_projection(Vnew)
+    # data_io.save_matrix(Vproj, 'savedProj.txt')
+
+    X = data_io.load_matrix('savedProj.txt')
+    # print X.shape
+
+    #plot_popular(X, movie_names, movie_ratings)
+    #plot_highest_rated(X, movie_names, movie_ratings)
+    #plot_genres(X, movie_names, movie_ratings, genres)
+    plot_handpicked(X, movie_names, movie_ratings)
+    plot_star(X, movie_names, movie_ratings)
+
+    try:
+        diag.show_plots()
+    except:
+        pass
